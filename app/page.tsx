41bf44fceaa442f91f53ced6e71643a476ad9a43
Faraskousei <farakousei42@gmail.com>
2025-09-29 10:06:44 +0700

    Ubah UI

--- app/page.tsx
+++ app/page.tsx
@@ -163,7 +163,6 @@ export default function Home() {
             </div>
           </div>
         </div>
-      </div>
 
         <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
           {/* Stats Cards */}
